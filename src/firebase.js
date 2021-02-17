7c4d569e408e819433d56ef17d5a93f85072b7ca
Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-02-17 19:16:47 +0100

    Enter

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyBmoJYgZmNOQ0HSt2x_j3QvjqEzc75nVUw",
+    authDomain: "facebook-clone-caa4e.firebaseapp.com",
+    projectId: "facebook-clone-caa4e",
+    storageBucket: "facebook-clone-caa4e.appspot.com",
+    messagingSenderId: "913262798558",
+    appId: "1:913262798558:web:2f5ad5ea469e86ca0d044f"
+  };
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db=firebaseApp.firestore()
+  const auth=firebase.auth()
+  const provider=new firebase.auth.GoogleAuthProvider();
+  export {auth,provider}
+  export default db
