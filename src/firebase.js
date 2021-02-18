b3e814b96b257832a9a05ee78c6a8d1b56198c60
Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-02-18 01:22:49 +0100

    Enter

--- src/firebase.js
+++ src/firebase.js
@@ -7,9 +7,10 @@ const firebaseConfig = {
     messagingSenderId: "913262798558",
     appId: "1:913262798558:web:2f5ad5ea469e86ca0d044f"
   };
-  const firebaseApp=firebase.initializeApp(firebaseConfig);
-  const db=firebaseApp.firestore()
-  const auth=firebase.auth()
-  const provider=new firebase.auth.GoogleAuthProvider();
-  export {auth,provider}
-  export default db
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
